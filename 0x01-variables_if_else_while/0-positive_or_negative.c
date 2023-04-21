38d99a8da9eeee8dd7edfc2b9bf9c95f0e13b206
slimouch <saloua.sarkadi@gmail.com>
2023-04-21 09:19:34 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,11 +18,11 @@ int main(void)
 	{
 		ptintf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	if (n == 0)
 	{
 		printf("%d is zero\n", n);
 	}
-	else
+	if (n < 0)
 	{
 		printf("%d is negative\n", n);
 	}
