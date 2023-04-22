cca267229781ac20979becced232778ff5a644b3
slimouch <saloua.sarkadi@gmail.com>
2023-04-21 18:01:08 -0700

    alphabet reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - a program that prints the alphabet in lowercase
+ *in reverse followed by a new line
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+int i = 26;
+while (i >= 0)
+{
+putchar(alpha[i]);
+i--
+}
+putchar('\n');
+return (0);
+}
