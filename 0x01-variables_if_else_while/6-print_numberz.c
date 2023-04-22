ab4b4629b0bb298758e68cd85d1f598a7a9f633d
slimouch <saloua.sarkadi@gmail.com>
2023-04-21 17:50:27 -0700

    num

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - a program that prints numbers
+ *followed by a new line
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
