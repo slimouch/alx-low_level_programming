ba1dfd014d27295d87d84aa54f2255284d8332a0
slimouch <saloua.sarkadi@gmail.com>
2023-04-25 19:21:12 -0700

    twointeger

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result.
+ * @n: the first integer to be computed
+ * @m: the second integer to be computed
+ * Return: Integer value
+ */
+int add(int n, int m)
+{
+return (n + m);
+}
