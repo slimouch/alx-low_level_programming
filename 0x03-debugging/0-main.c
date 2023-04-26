33604e48152ccba24789ae8687afcba910fbf5ec
slimouch <saloua.sarkadi@gmail.com>
2023-04-26 06:51:40 -0700

    test2

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * main - tests function that prints int if it is positive or negative
+ * main - tests function that prints int if it  is positive or negative
  * Return: 0
  */
 int main(void)
 {
 int n;
+
 n = 0;
 positive_or_negative(n);
 return (0);
