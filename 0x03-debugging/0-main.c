5b2c65afebaa38936eb36b958e7e03439b3d8e1e
slimouch <saloua.sarkadi@gmail.com>
2023-04-30 10:23:40 -0700

    debugging test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,9 @@
 #include "main.h"
 
 /**
-<<<<<<< HEAD
  * main - tests function if it is positive or negative
  * Return: 0
  */
-=======
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
->>>>>>> 4983521da700618557a2969760098f64e913083a
-
 int main(void)
 {
 int i;
