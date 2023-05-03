75605511e047b4acbe027e67456223fd42636970
slimouch <saloua.sarkadi@gmail.com>
2023-05-03 07:02:29 -0700

    task5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string.
+ * @s: the string
+ * Return: void
+ */
+void rev_string(char *s)
+{
+int l = 0, i = 0;
+char temp;
+while (s[i++])
+l++;
+for (i = l - 1; i >= l / 2; i--)
+{
+temp = s[i];
+s[i] = s[l - i - 1];
+s[l - i - 1] = tmp;
+}
+}
