0a1a4fd48057cdab46b835b04ff0bdae5b94dbd1
slimouch <saloua.sarkadi@gmail.com>
2023-05-03 08:57:02 -0700

    swp file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string,
+ * followed by a new line.
+ * @str: string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int a = 0, b;
+while (str[])
+}
