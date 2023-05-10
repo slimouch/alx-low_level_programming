0485910131991f26aac6adb06e5ce6366070684b
slimouch <128979583+slimouch@users.noreply.github.com>
2023-05-10 19:43:50 +0100

    Create 6-is_prime_number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+int verify_p(int n, int v);
+
+/**
+
+ * is_prime_number - returns 1 if it is a prime number, otherwise return 0.
+
+ * @n: the integer
+
+ * Return: integer
+
+*/
+
+int is_prime_number(int n)
+
+{
+
+return (verify_p(n, 1));
+
+}
+
+/**
+
+ * verify_p - check if the input is a prime number
+
+ * @n: integer
+
+ * @v: integer
+
+ * Return: integer
+
+*/
+
+int verify_p(int n, int v)
+
+{
+
+if (n <= 1)
+
+return (0);
+
+else if (n % v == 0 && v > 1)
+
+return (0);
+
+else if ((n / v) < v)
+
+return (1);
+
+return (verify_p(n, v + 1));
+
+}
