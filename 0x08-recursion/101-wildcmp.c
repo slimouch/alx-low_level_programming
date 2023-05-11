1d8c905d7be36efaa2a878fa7dafd5a135bc14ee
slimouch <128979583+slimouch@users.noreply.github.com>
2023-05-11 02:43:55 +0100

    Update last

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,9 +12,8 @@ return (0);
 else if (*s1 == '\0' && *s2 == '\0')
 return (1);
 else if (*s1 == *s2)
-return (wildcmp(s1 +1, s2 + 1));
+return (wildcmp(s1 + 1, s2 + 1));
 else if (*s2 == '*')
-return (wildcmp(s1, s2 + 1) || wildcmp(s1 +1, s2));
+return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
 return (0);
-}
-	
+}	
