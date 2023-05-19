c1dbce0f3b08d94b4066a90d1dec0f283eb795bf
slimouch <saloua.sarkadi@gmail.com>
2023-05-19 09:32:15 -0700

    task3

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,42 +1,37 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * *string_nconcat - concatenates two strings.
- * @s1: first string
- * @s2: second string
- * @n: integer
- * Return: a pointer
+ * memor - to fill memory.
+ * @ms: string
+ * @scp: string
+ * @a: integer
+ * Return: a pointer to the memory.
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+char *memor(char *ms, char scp, unsigned int a)
+{
+	unsigned int j;
+
+	for (j = 0; j < a; j++)
+	{
+		ms[j] = scp;
+	}
+	return (ms);
+}
+/**
+ * *_calloc - allocates memory for an array, using malloc.
+ * @nmemb: integer
+ * @size: integer
+ * Return: a pointer to the allocated memory.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *str;
-	unsigned int a = 0, b = 0, l1 = 0, l2 = 0;
 
-	while (s1 && s1[l1])
-	{
-		l1++;
-	}
-	while (s2 && s2[l2])
-	{
-		l2++;
-	}
-	if (n < l2)
-	{
-		str = malloc(sizeof(char) * (l1 + n + 1));
-	}
-	else
-		str = malloc(sizeof(char) * (l1 + l2 + 1));
-	if (!str)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-	while (a < l1)
-	{
-		str[a] = s1[a];
-		a++;
-	}
-	while (n < l2 && a < (l1 + n))
-		str[a++] =  s2[b++];
-	while (n >= l2 && a < (l1 + l2))
-		str[a++] = s2[b++];
-	str[a] = '\0';
+	str = malloc(size * nmemb);
+	if (str == NULL)
+		return (NULL);
+	memor(str, 0, nmemb * size);
 	return (str);
 }
