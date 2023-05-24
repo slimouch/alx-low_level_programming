dd7c3cc215a092395608b8e9f6ed07c70a68af3e
slimouch <saloua.sarkadi@gmail.com>
2023-05-24 12:52:20 -0700

    task3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+/**
+ * main - ti verify the code
+ * @argc: the num of arg
+ * @argv: th earg array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int (*op_func)(int, int), a, b;
+
+	if (argc != 4)
+		printf("Error\n"), exit(98);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	op_func = get_op_func(argv[2]);
+	if (!op_func)
+		printf("Error\n"), exit(99);
+	if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
+		printf("Error\n"), exit(100);
+	printf("%d\n", op_func(a, b));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - add 2 intgers
+ * @a: the 1 int
+ * @b: the 2 int
+ * Return: summ
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - to sub 2 intgers
+ * @a: the 1 int
+ * @b: the 2 int
+ * Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - to multi 2 intgers
+ * @a: the 1 int
+ * @b: the 2 int
+ * Return: mult
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division of 2 intgers
+ * @a: the 1 int
+ * @b: the 2 int
+ * Return: divion
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - modulo 2 intgers
+ * @a: the 1 int
+ * @b: the 2 int
+ * Return: modulo
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
