bb2589100f170778dbd40d93d8e5481a8f2474ef
slimouch <saloua.sarkadi@gmail.com>
2023-05-24 19:55:02 -0700

    tt

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main
+ * @argc: arguments
+ * @argv: array of args
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	char *s = (char *)main;
+	int t;
+	
+	if (argc != 2)
+		printf("Error\n"), exit(1);
+	t = atoi(argv[1]);
+	if (t < 0)
+		printf("Error\n"), exit(2);
+	while (t--)
+		printf("%02hhx%s", *s++, t ? " " : "\n");
+	return (0);
+}
