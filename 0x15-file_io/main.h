3e3341e23955a6a63b9520ea5ea45b5d42a1b3f3
slimouch <saloua.sarkadi@gmail.com>
2023-06-12 18:45:24 -0700

    task3

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -9,4 +10,6 @@ char _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+char *CreatBuff(char *filename);
+void CloseF(int length);
 #endif
