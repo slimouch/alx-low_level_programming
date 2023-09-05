b72bb516266556b2a016f1a30e397fb8a96497b1
slimouch <saloua.sarkadi@gmail.com>
2023-09-04 17:41:05 -0700

    t3 up

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,78 +1,76 @@
 #include "main.h"
 /**
  * CreatBuff - to create a buffer
- * @filename: the file name
+ * @filename: the name of the file buffer
  * Return: a pointer to the new file buffer
  */
 char *CreatBuff(char *filename)
 {
-	char *buffer;
+	char *bf;
 
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
+	bf = malloc(sizeof(char) * 1024);
+	if (bf == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	exit(99);
 	}
-	return (buffer);
+	return (bf);
 }
 /**
- * CloseF - close a file descriptor
- * @desc: the closed file descriptor
+ * CloseF - to close a file descriptor
+ * @desc: the closed file descriptor.
  * Return: void
  */
 void CloseF(int desc)
 {
-	int f;
+	int F;
 
-	f = close(desc);
-	if (f == -1)
+	F = close(desc);
+	if (F == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd FD_VALUE %d\n", desc);
-		exit(100);
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", desc);
+	exit(100);
 	}
 }
 /**
- * main - copy the file contents to another
- * @argc: the number of arguments
+ * main - Copy the file contents to another
+ * @argc: the number of arg
  * @argv: an array
- * Return: 0 success
+ * Return: 0 on success.
  */
 int main(int argc, char *argv[])
 {
-	int w, r, dsc, a;
-	char *bff;
+	int R, W, De, A;
+	char *buf;
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	bff = CreatBuff(argv[2]);
-	dsc = open(argv[1], O_RDONLY);
-	r = read(dsc, bff, 1024);
-	a = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buf = CreatBuff(argv[2]);
+	De = open(argv[1], O_RDONLY);
+	R = read(De, buf, 1024);
+	A = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	do {
-		if (dsc == -1 || r == -1)
+		if (De == -1 || R == -1)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't read from file
-				NAME_OF_THE_FILE %s\n", argv[1]);
-		free(bff);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		free(buf);
 		exit(98);
 		}
-		w = write(a, bff, r);
-		if (a == -1 || w == -1)
+		W = write(A, buf, R);
+		if (A == -1 || W == -1)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't write to
-				NAME_OF_THE_FILE %s\n", argv[2]);
-		free(bff);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		free(buf);
 		exit(99);
 		}
-		r = read(dsc, bff, 1024);
-		a = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
-	free(bff);
-	CloseF(dsc);
-	CloseF(a);
+		R = read(De, buf, 1024);
+		A = open(argv[2], O_WRONLY | O_APPEND);
+	} while (R > 0);
+	free(buf);
+	CloseF(De);
+	CloseF(A);
 	return (0);
 }
