9dea1e3207a27515189f218dc7907f9d4dc7717a
slimouch <128979583+slimouch@users.noreply.github.com>
2023-09-28 23:50:39 +0100

    Update 1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: int
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+  unsigned long int numero;
+  int i = 63;
+  int cmp = 0;
+
+  while (i >= 0)
+  {
+    numero = n >> i;
+    i--;
+    if (numero & 1)
+    {
+      _putchar('1');
+      cmp++;
+    }
+    else if (cmp)
+      _putchar('0');
+  }
+  if (!cmp)
+    _putchar('0');
+}
